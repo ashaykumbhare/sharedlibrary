a6bde843bd6396dab7786e6aa9e01dd74aaee30d
ashaykumbhare <127437585+ashaykumbhare@users.noreply.github.com>
2023-03-20 19:14:47 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -4,5 +4,13 @@ def newDownload(repo)
 }
 def newBuild()
 {
-  sh 'mvn package'
+  sh "mvn package"
+}
+def newDeploy(ip,path)
+{
+  deploy adapters: [tomcat9(credentialsId: '80e1d619-bdae-49ae-b6c8-dcb12c9b296a', path: '', url: 'http://${ip}:8080')], contextPath: '${path}', war: '**/*.war'
+}
+def newTest(testpipeline)
+{
+  sh "java -jar /var/lib/jenkins/workspace/$(testpipeline}/testing.jar"
 }
