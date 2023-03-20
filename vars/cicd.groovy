7c6409f770b215edba906dc6bf64576fd02e4389
ashaykumbhare <127437585+ashaykumbhare@users.noreply.github.com>
2023-03-20 19:30:35 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,7 @@ def newBuild()
 }
 def newDeploy(ip,path)
 {
-  deploy adapters: [tomcat9(credentialsId: '80e1d619-bdae-49ae-b6c8-dcb12c9b296a', path: '', url: 'http://'${ip}':8080')], contextPath: ${path}, war: '**/*.war'
+  deploy adapters: [tomcat9(credentialsId: '80e1d619-bdae-49ae-b6c8-dcb12c9b296a', path: '', url: "http://"${ip}":8080")], contextPath: ${path}, war: '**/*.war'
 }
 def newTest(testpipeline)
 {
