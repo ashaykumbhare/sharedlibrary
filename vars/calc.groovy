fc27d559d97282a094913e524be3709d787308d0
ashaykumbhare <127437585+ashaykumbhare@users.noreply.github.com>
2023-03-20 18:35:34 +0530

    Update calc.groovy

--- vars/calc.groovy
+++ vars/calc.groovy
@@ -8,7 +8,7 @@ echo "The subtraction of ${a} and ${b} is ${a-b}"
 }
 def multi(a,b)
 {
-echo "The multi of ${a} and ${b} is ${a*b}"
+echo "The multiplication of ${a} and ${b} is ${a*b}"
 }
 def div(a,b)
 {
